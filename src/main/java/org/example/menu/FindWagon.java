8ca05088d5e89f230e9b0f2c383fccb06dc8fbd4
yurii <dzhebkoyurii@gmail.com>
2024-11-06 20:45:46 +0200

    Made FindWagon ready. Full ready program

--- src/main/java/org/example/menu/FindWagon.java
+++ src/main/java/org/example/menu/FindWagon.java
@@ -1,9 +1,10 @@
 package org.example.menu;
 import org.example.objects.*;
 
-import java.util.List;
+import java.util.Scanner;
 
 public class FindWagon implements MenuItem {
+    private static Scanner scanner = new Scanner(System.in);
     Train train;
 
     public FindWagon(Train train) {
@@ -11,7 +12,29 @@ public class FindWagon implements MenuItem {
     }
 
     @Override
-    public void execute(){}
+    public void execute(){
+        if (train == null || train.getWagons().isEmpty()) {
+            System.out.println("Поїзда чи вагонів не існує.");
+            return;
+        }
+
+        System.out.print("Введіть мінімальну кількість пасажирів: ");
+        int minPassengers = scanner.nextInt();
+        System.out.print("Введіть максимальну кількість пасажирів: ");
+        int maxPassengers = scanner.nextInt();
+
+
+        boolean check = false;
+        for (Wagon wagon : train.getWagons()){
+            if (wagon.getCapacity() > minPassengers && wagon.getCapacity() < maxPassengers){
+                System.out.println(wagon);
+                check = true;
+            }
+        }
+        if (!check){
+            System.out.println("Таких вагонів немає!");
+        }
+    }
 
     @Override
     public String getName() {
